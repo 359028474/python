903acfb99d5754e743aad123cbb412c720087c63
zhangjian <359028474@qq.com>
2019-11-13 10:09:06 +0800

    代码提交

--- venv/ChinaSoft/export_users.sh
+++ venv/ChinaSoft/export_users.sh
@@ -2,14 +2,14 @@
 
 if [ $2 -ge 999999 ]
 then
-        sleep 15
+  sleep 15
 else
-        sleep 3
+  sleep 3
 fi
 row_num=` wc -l $1 | awk -F " " '{print $1}'`
 while (( $2 != $row_num ))
 do
-        row_num=` wc -l $1 | awk -F " " '{print $1}'`
+  row_num=` wc -l $1 | awk -F " " '{print $1}'`
 done
 cat $1 | awk -F "," '{print $1}' | sed -n 'p' >> $3.csv
 res=` wc -l $3.csv | awk -F " " '{print $1}'`
