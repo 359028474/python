f568a75e1329da3681d540bc04078ce3d6c75421
zhangjian <359028474@qq.com>
2019-11-22 15:25:55 +0800

    代码提交

--- venv/ChinaSoft/Demo.py
+++ venv/ChinaSoft/Demo.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
-print('hello world')
-print("I'm %s. I'm %d year old" % ('Vamei', 99))
-
-statis_month = 201911
-sql_1 = '''
-insert overwrite table dm.table_name partition(statis_month=%(statis_month)s)
-''' % { 'statis_month': statis_month}
-print(sql_1)
+# print('hello world')
+# print("I'm %s. I'm %d year old" % ('Vamei', 99))
+#
+# statis_month = 201911
+# sql_1 = '''
+# insert overwrite table dm.table_name partition(statis_month=%(statis_month)s)
+# ''' % { 'statis_month': statis_month}
+# print(sql_1)
+'''---------------test---------------'''
+f = open('D:\\test.txt')
+t = f.readline()
+print(type(t))
+print(t)
